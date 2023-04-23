4a424fb68555a35c6f8bc3634abb1ce4fb187245
bircha90 <122287734+bircha90@users.noreply.github.com>
2023-04-23 12:46:18 -0400

    Update Tic-Tac-Toe Gui.py

--- Tic-Tac-Toe Gui.py
+++ Tic-Tac-Toe Gui.py
@@ -36,19 +36,16 @@ def CheckWin():
         Game = Win    
     elif(board[7] == board[8] and board[8] == board[9] and board[7] != ' '):    
         Game = Win    
-    #Vertical Winning Condition    
     elif(board[1] == board[4] and board[4] == board[7] and board[1] != ' '):    
         Game = Win    
     elif(board[2] == board[5] and board[5] == board[8] and board[2] != ' '):    
         Game = Win    
     elif(board[3] == board[6] and board[6] == board[9] and board[3] != ' '):    
-        Game=Win    
-    #Diagonal Winning Condition    
+        Game=Win     
     elif(board[1] == board[5] and board[5] == board[9] and board[5] != ' '):    
         Game = Win    
     elif(board[3] == board[5] and board[5] == board[7] and board[5] != ' '):    
-        Game=Win    
-    #Match Tie or Draw Condition    
+        Game=Win      
     elif(board[1]!=' ' and board[2]!=' ' and board[3]!=' ' and board[4]!=' ' and board[5]!=' ' and board[6]!=' ' and board[7]!=' ' and board[8]!=' ' and board[9]!=' '):    
         Game=Draw    
     else:            
