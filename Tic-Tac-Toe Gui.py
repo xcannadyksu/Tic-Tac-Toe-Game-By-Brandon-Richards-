796a2d932bec0cd7f8b93f322d1d68281f3c33e5
bircha90 <122287734+bircha90@users.noreply.github.com>
2023-04-24 16:45:24 -0400

    Update Tic-Tac-Toe Gui.py

--- Tic-Tac-Toe Gui.py
+++ Tic-Tac-Toe Gui.py
@@ -1,83 +1,47 @@
-import os    
-import time    
-    
-board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']    
-player = 1    
-   
-Win = 1    
-Draw = -1    
-Running = 0    
-Stop = 1    
-  
-Game = Running    
-Mark = 'X'    
-   
-  
-def DrawBoard():    
-    print(" %c | %c | %c " % (board[1],board[2],board[3]))    
-    print("___|___|___")    
-    print(" %c | %c | %c " % (board[4],board[5],board[6]))    
-    print("___|___|___")    
-    print(" %c | %c | %c " % (board[7],board[8],board[9]))    
-    print("   |   |   ")    
-    
-def CheckPosition(x):    
-    if(board[x] == ' '):    
-        return True    
-    else:    
-        return False    
-   
-def CheckWin():    
-    global Game 
-    if(board[1] == board[2] and board[2] == board[3] and board[1] != ' '):    
-        Game = Win    
-    elif(board[4] == board[5] and board[5] == board[6] and board[4] != ' '):    
-        Game = Win    
-    elif(board[7] == board[8] and board[8] == board[9] and board[7] != ' '):    
-        Game = Win     
-    elif(board[1] == board[4] and board[4] == board[7] and board[1] != ' '):    
-        Game = Win    
-    elif(board[2] == board[5] and board[5] == board[8] and board[2] != ' '):    
-        Game = Win    
-    elif(board[3] == board[6] and board[6] == board[9] and board[3] != ' '):    
-        Game=Win       
-    elif(board[1] == board[5] and board[5] == board[9] and board[5] != ' '):    
-        Game = Win    
-    elif(board[3] == board[5] and board[5] == board[7] and board[5] != ' '):    
-        Game=Win     
-    elif(board[1]!=' ' and board[2]!=' ' and board[3]!=' ' and board[4]!=' ' and board[5]!=' ' and board[6]!=' ' and board[7]!=' ' and board[8]!=' ' and board[9]!=' '):    
-        Game=Draw    
-    else:            
-        Game=Running 
-    
-print("Tic-Tac-Toe Game")    
-print("Player 1 [X] --- Player 2 [O]\n")    
-print()    
-print()    
-print("Please Wait...")    
-time.sleep(3)    
-while(Game == Running):    
-    os.system('cls')    
-    DrawBoard()    
-    if(player % 2 != 0):    
-        print("Player 1's chance")    
-        Mark = 'X'    
-    else:    
-        print("Player 2's chance")    
-        Mark = 'O'    
-    choice = int(input("Enter the position between [1-9] where you want to mark : "))    
-    if(CheckPosition(choice)):    
-        board[choice] = Mark    
-        player+=1    
-        CheckWin()    
-    
-os.system('cls')    
-DrawBoard()    
-if(Game==Draw):    
-    print("Game Draw")    
-elif(Game==Win):    
-    player-=1    
-    if(player%2!=0):    
-        print("Player 1 Won")    
-    else:    
-        print("Player 2 Won") 
+
+def print_board(board):
+    print("-------------")
+    print("| " + board[0] + " | " + board[1] + " | " + board[2] + " |")
+    print("-------------")
+    print("| " + board[3] + " | " + board[4] + " | " + board[5] + " |")
+    print("-------------")
+    print("| " + board[6] + " | " + board[7] + " | " + board[8] + " |")
+    print("-------------")
+
+def check_win(board, player):
+    if ((board[0] == player and board[1] == player and board[2] == player) or
+        (board[3] == player and board[4] == player and board[5] == player) or
+        (board[6] == player and board[7] == player and board[8] == player) or
+        (board[0] == player and board[3] == player and board[6] == player) or
+        (board[1] == player and board[4] == player and board[7] == player) or
+        (board[2] == player and board[5] == player and board[8] == player) or
+        (board[0] == player and board[4] == player and board[8] == player) or
+        (board[2] == player and board[4] == player and board[6] == player)):
+        return True
+    else:
+        return False
+
+def main():
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    players = ["X", "O"]
+    current_player = players[0]
+    while True:
+        print_board(board)
+        print("Player", current_player)
+        move = int(input("Enter a number from 1-9: "))
+        if board[move-1] != " ":
+            print("That move is already taken. Try again.")
+            continue
+        board[move-1] = current_player
+        if check_win(board, current_player):
+            print_board(board)
+            print("Player", current_player, "wins!")
+            break
+        if " " not in board:
+            print_board(board)
+            print("Tie game!")
+            break
+        current_player = players[(players.index(current_player) + 1) % 2]
+
+if __name__ == "__main__":
+    main()
